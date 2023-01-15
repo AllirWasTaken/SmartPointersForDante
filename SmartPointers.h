b06ed2c2011718cfa97007c1662dbf6a66b2f7d2
Allir <97488183+AllirWasTaken@users.noreply.github.com>
2023-01-15 11:45:39 +0100

    Version 1.2

--- SmartPointers.h
+++ SmartPointers.h
@@ -4,10 +4,10 @@
 #define POINTER_LIMIT 65536
 #define DEFAULT_ALLOCATION_LIMIT 1844674407370955161
 
-#define malloc(x) s_malloc(x,__func__,__LINE__)
-#define calloc(x,y) s_calloc(x,y,__func__,__LINE__)
-#define free(x) s_free(x)
-#define realloc(x,y) s_realloc(x,y,__func__,__LINE__)
+#define malloc(size) s_malloc(size,__func__,__LINE__)
+#define calloc(numberOfElements,sizeOfElement) s_calloc(x,y,__func__,__LINE__)
+#define free(adres) s_free(x)
+#define realloc(memory,newSize) s_realloc(x,y,__func__,__LINE__)
 
 
 void *s_malloc(size_t size, const char* func,size_t line);
